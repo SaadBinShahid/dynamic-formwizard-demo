eb4cdccbf2685a947e5be3e6b54c7b29b5406c4c
Steven Cummings <estebistec@gmail.com>
2014-08-23 14:32:58 -0500

    Dynamically provide the wizard forms

--- demo/views.py
+++ demo/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 
 
+from collections import OrderedDict
 import logging
 
 from django.contrib.formtools.wizard.views import SessionWizardView
+from django.contrib.formtools.wizard.views import WizardView
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
@@ -17,6 +19,20 @@ logger = logging.getLogger(__name__)
 
 class ContactWizard(SessionWizardView):
     template_name = 'demo/wizard.html'
+    initial_dict = {}
+    instance_dict = {}
+    condition_dict = {}
+
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        return super(WizardView, cls).as_view(*args, **kwargs)
+
+    @property
+    def form_list(self):
+        return OrderedDict([
+            ('contact1', ContactForm1),
+            ('contact2', ContactForm2)
+        ])
 
     def done(self, form_list, **kwargs):
         for form in form_list:
@@ -24,5 +40,5 @@ class ContactWizard(SessionWizardView):
         return HttpResponseRedirect(reverse('done'))
 
 
-contact_wizard = ContactWizard.as_view([ContactForm1, ContactForm2])
+contact_wizard = ContactWizard.as_view()
 done = TemplateView.as_view(template_name='demo/done.html')
